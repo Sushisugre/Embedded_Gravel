bf77fd2858e2d988f64d789c01d6b9805f065d42
Sushi <just.s444@gmail.com>
2015-10-03 02:29:00 -0400

    try to avoid the rearrangement of register r3

--- lab1-part2-shis/part2asm/strTable.s
+++ lab1-part2-shis/part2asm/strTable.s
@@ -15,13 +15,12 @@ strTable:
 	@ args = 0, pretend = 0, frame = 0
 	@ frame_needed = 0, uses_anonymous_args = 0
 	stmfd	sp!, {r4, r5}
-@	mov	r5, r0  @ r0 = src
-	mov	r5, r3  @ r5 = r3 = dl
-	subs r4, r5, #1  @ array index r4 = dl - 1
+@	mov	r5, r3  @ r5 = r3 = dl
+	subs r4, r3, #1  @ array index r4 = dl - 1, r3 = dl
 	blt	.L11  @ dl - 1 should >=0, dl >= 1
 .L9:
-	ldrb	r3, [r1, r4]	@ zero_extendqisi2 @ r3 = each character in dst 
-	mov ip, r3
+	ldrb	r5, [r1, r4]	@ zero_extendqisi2 @ r3 = each character in dst 
+	mov ip, r5
 .L7:
 	sub	ip, ip, #23
 	cmp	ip, #22
@@ -29,7 +28,7 @@ strTable:
 .L13:
 	cmp	ip, r2 @ r2 = sl
 	bgt	.L4  @ if position ip > sl, ignore
-	strb	r3, [r0, ip] @ r0 = src
+	strb	r5, [r0, ip] @ r0 = src
 .L4:
 	subs r4, r4, #1  @ r4--
 	bge	.L9  @ while r4 >= 0
