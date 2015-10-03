79f943d53855df1e48f889db882cc75be7322fd6
Sushi <just.s444@gmail.com>
2015-10-03 02:35:35 -0400

    test get rid of r5

--- lab1-part2-shis/part2asm/strTable.s
+++ lab1-part2-shis/part2asm/strTable.s
@@ -18,8 +18,8 @@ strTable:
 	subs r4, r3, #1  @ array index r4 = dl - 1, r3 = dl
 	blt	.L11  @ dl - 1 should >=0, dl >= 1
 .L9:
-	ldrb	r5, [r1, r4]	@ zero_extendqisi2 @ r3 = each character in dst 
-	mov ip, r5
+	ldrb	ip, [r1, r4]	@ zero_extendqisi2 @ r3 = each character in dst 
+@	mov ip, r5
 .L7:
 	sub	ip, ip, #23
 	cmp	ip, #22
@@ -27,7 +27,7 @@ strTable:
 .L13:
 	cmp	ip, r2 @ r2 = sl
 	bgt	.L4  @ if position ip > sl, ignore
-	strb	r5, [r0, ip] @ r0 = src
+	strb	ip, [r0, ip] @ r0 = src
 .L4:
 	subs r4, r4, #1  @ r4--
 	bge	.L9  @ while r4 >= 0
