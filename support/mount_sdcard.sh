b8a62ab7410657730584be98cccb6b5c86126d17
Sushi <just.s444@gmail.com>
2015-10-08 11:14:01 -0400

    update mount sdcard addres

--- support/mount_sdcard.sh
+++ support/mount_sdcard.sh
@@ -1,3 +1,3 @@
-sudo losetup /dev/loop0 ~/workspace/embedded/lab1-support/sdcard.img
+sudo losetup /dev/loop0 ~/workspace/embedded/support/sdcard.img
 sudo kpartx -a /dev/loop0
 sudo mount /dev/mapper/loop0p1 /media/bootfs
