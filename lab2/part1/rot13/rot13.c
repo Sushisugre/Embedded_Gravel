83cc594d8268264edd53148331487b7764689001
Sushi <just.s444@gmail.com>
2015-10-13 11:41:18 -0400

    handle variable input

--- lab2/part1/rot13/rot13.c
+++ lab2/part1/rot13/rot13.c
@@ -3,51 +3,68 @@
  *
  * Authors: Shi Su <shis@andrew.cmu.edu>
  *          
- * Date:    The current time & date
+ * Date:    Tue Oct 13 11:39:59 EDT 2015
  */
 
  #include <unistd.h>
  #include <stdlib.h>
  #include <errno.h>
 
+ typedef int bool;
  #define true 1
  #define false 0
  #define CYCLE_SIZE 26
- #define BUFFER_SIZE 100
+ #define BUFFER_SIZE 10
 
  void rot(unsigned char* letter, int offset);
 
  int main(int argc, char *argv[]) {
 
     // use gdb to check argc, argv
+    // may overflow if use signed char
+    unsigned char buffer[BUFFER_SIZE];
+
+    // loop and wait for input
     while(true){
-        // may overflow if use signed char
-        unsigned char buffer[BUFFER_SIZE];
-        int num = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+        bool is_finished = false;
+
+        // loop until finish reading all the inputs
+        while(is_finished != true){
+
+            int num = read(STDIN_FILENO, buffer, BUFFER_SIZE);
 
         // terminate when zere byte read
         // or interrupted by a signal
-        if(num == 0){
-            exit(0);
-        }
+            if(num == 0){
+                exit(0);
+            }
         // syscall error
-        if(num == -1){
-            exit(1);
-        }
+            if(num == -1){
+                exit(1);
+            }
 
-        // interate through the characters
-        unsigned char *p = buffer;
-        while (*p != '\0') {
-            rot(p, 13);
-            p++;
-        }
+            if(num < BUFFER_SIZE
+                || (num == BUFFER_SIZE && buffer[BUFFER_SIZE - 1]== '\0')){
+                is_finished = true;
+            }
+
+            // interate through the characters
+            int i = 0;
+            unsigned char *p = buffer;
+            while (*p != '\0' && i < BUFFER_SIZE) {
+                rot(p, 13);
+                p++;
+                i++;
+            }
 
         // print out rotated array
-        num = write(STDOUT_FILENO ,buffer, num);
+            num = write(STDOUT_FILENO ,buffer, num);
         // syscall error
-        if(num == -1){
-            exit(1);
+            if(num == -1){
+                exit(1);
+            }
         }
+
 
     }
     return 0;
@@ -68,9 +85,9 @@ void rot(unsigned char* letter, int offset){
     else if('a' <= x && x <= 'z'){
         x = x + offset;
         if(x > 'z'){
-         x = x - CYCLE_SIZE;
-     }
- }
+           x = x - CYCLE_SIZE;
+       }
+   }
 
- *letter = x;
+   *letter = x;
 }
