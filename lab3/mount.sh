7ca6dd7a9e03f0e9a12f23c2949f239fd26945b0
Sushi <just.s444@gmail.com>
2015-11-04 10:49:35 -0500

    fix mount script

--- lab3/mount.sh
+++ lab3/mount.sh
@@ -2,8 +2,31 @@
 echo "making"
 make
 echo "mounting mmc1"
-mount /mnt/mmc1
-cp kernel/kernel.bin /mnt/mmc1
-cp tasks/bin/*.bin /mnt/mmc1
-umount /mnt/mmc1
+# mount /mnt/mmc1
+# cp kernel/kernel.bin /mnt/mmc1
+# cp tasks/bin/*.bin /mnt/mmc1
+# umount /mnt/mmc1
+
+MOUNT_FOLDER = /media/bootfs
+
+sudo losetup /dev/loop1 $SUPPORT_FOLDER/sdcard.img
+sudo kpartx -a /dev/loop1
+sudo mount /dev/mapper/loop1p1 $MOUNT_FOLDER
+
+if [ -z "$var" ];
+then
+    sudo cp kernel/kernel.bin $MOUNT_FOLDER
+    sudo cp tasks/bin/*.bin $MOUNT_FOLDER
+else
+    for var
+    do
+     sudo cp $var $MOUNT_FOLDER
+    done
+fi
+sync;sync;sync
+sudo umount /media/bootfs
+sudo kpartx -d /dev/loop1
+sudo losetup -d /dev/loop1
+
 echo "done mounting"
+
