fbd2bb8f95b1bff1bdfe38b7eeff2a358b4d82b2
Mengjin <mengjin@mengjin-VirtualBox.(none)>
2015-11-06 02:01:12 -0500

    add clear ossr bit

--- lab3/kernel/time_driver.c
+++ lab3/kernel/time_driver.c
@@ -21,6 +21,9 @@ void time_driver() {
   
   // Set the counter register to zero
   reg_write(OSTMR_OSCR_ADDR, 0x0);
+  
+  // Clear OSSR
+  reg_set(OSTMR_OSSR_ADDR, OSTMR_OSSR_M0);
 
   // Update the counters
   g_ms_counter = g_ms_counter + 10;
