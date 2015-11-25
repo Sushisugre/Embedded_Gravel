752ed3c6f5754d3f5ad1e294e3ea2f9bbcf045f8
Sushi <just.s444@gmail.com>
2015-11-25 13:59:23 -0500

    test move typo to lab4

---
+++ lab4/tasks/typo/typo.c
@@ -0,0 +1,50 @@
+/** @file typo.c
+ *
+ * @brief Echos characters back with timing data.
+ *
+ * Links to libc.
+ *
+ * @author Mengjin Yan <mengjinyan@cmu.edu>
+ *         Shi Su <shis@andrew.cmu.edu>
+ * 
+ * @date   Fri Nov  6 02:53:01 EST 2015 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <bits/fileno.h>
+
+#define BUFFER_SIZE 50
+
+int main(int argc, char** argv)
+{
+  unsigned char buffer[BUFFER_SIZE];
+  int read_size;
+  unsigned long start_time, end_time;
+  //double duration; 
+  unsigned duration;
+  unsigned second, fraction;
+
+  while(1) {
+    printf("Type Something and Press Enter:\n");
+    start_time = time();
+    read_size = read(STDIN_FILENO, buffer, BUFFER_SIZE);    
+    end_time = time();
+    
+    //duration = ((double)(end_time - start_time))/1000.0;
+    duration = end_time - start_time;
+    second = ((unsigned)duration)/1000;
+    fraction = (duration/100)%10;
+
+    if(write(STDOUT_FILENO, buffer, read_size) < 0)
+      exit(1);
+    // printf("%.1fs\n", duration);
+    printf("%d.%ds\n", second, fraction);
+
+    if(read_size < 0)
+      exit(1);
+  }
+
+  return 0;
+}
