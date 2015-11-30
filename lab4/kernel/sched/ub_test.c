55f6c2e6d977ebabf8e4320a1e780736f673d89f
Mengjin Yan <yanmengjin1@YanMengjindeMBP.wv.cc.cmu.edu>
2015-11-29 23:40:11 -0500

    add ub test and sort part

--- lab4/kernel/sched/ub_test.c
+++ lab4/kernel/sched/ub_test.c
@@ -13,7 +13,21 @@
 #include <exports.h>
 #endif
 
- 
+/**
+ * the array to store the u(n) value of the ub test
+ * for better performance
+ */ 
+static float u_n_num[64] = {
+	1.000, 0.828, 0.780, 0.757, 0.743, 0.735, 0.729, 0.724,
+	0.721, 0.718, 0.715, 0.714, 0.712, 0.711, 0.709, 0.708, 
+	0.707, 0.707, 0.706, 0.705, 0.705, 0.704, 0.704, 0.703, 
+	0.703, 0.702, 0.702, 0.702, 0.701, 0.701, 0.701, 0.701, 
+	0.700, 0.700, 0.700, 0.700, 0.700, 0.700, 0.699, 0.699, 
+	0.699, 0.699, 0.699, 0.699, 0.699, 0.698, 0.698, 0.698, 
+	0.698, 0.698, 0.698, 0.698, 0.698, 0.698, 0.698, 0.697, 
+	0.697, 0.697, 0.697, 0.697, 0.697, 0.697, 0.697, 0.697
+};
+
 /**
  * @brief Perform UB Test and reorder the task list.
  *
@@ -29,8 +43,26 @@
  */
 int assign_schedule(task_t** tasks  __attribute__((unused)), size_t num_tasks  __attribute__((unused)))
 {
+	int i;
+	float result = 0;
 
-	return 1; // fix this; dummy return to prevent compiler warnings	
+	// do ub test
+	// calculate the result on the left side of the equation
+	for(i = 0; i < num_tasks; i++) {
+		result = result + ((float)(tasks[i]->C))/((float)(tasks[i]->T));
+	}
+
+	// check the value
+	if(result <= u_n_num[num_tasks-1]) {
+		// if pass ub test, then sort the tasks according to the priority
+		sort_tasks(tasks, num_tasks);
+		return 1;
+	} else {
+		// if not pass ub test
+		return 0;
+	}
+
+	//return 1; // fix this; dummy return to prevent compiler warnings	
 }
 	
 /**
@@ -38,9 +70,20 @@ int assign_schedule(task_t** tasks  __attribute__((unused)), size_t num_tasks  _
  * @param tasks An array of task pointers containing the task set to schedule.
  */
 void sort_tasks(task_t** tasks  __attribute__((unused)), size_t num_tasks  __attribute__((unused))) {
-    size_t out;
-    for (out = 0; out < num_tasks; out++)
-    {
-        //task_t tmp = 
+    size_t i, j, k;
+    task_t* temp;
+
+    // insertion sort
+    for (i = 0; i < num_tasks; i++) {
+    	for(j = 0; j < i; j++) {
+    		if((tasks[i]->cur_prio) < (tasks[j]->cur_prio)) {
+    			temp = tasks[i];
+    			for(k = i-1; k >= j; k--) {
+    				tasks[k+1] = tasks[k];
+    			}
+    			tasks[j] = temp;
+    			break;
+    		}
+    	}
     }
 }
